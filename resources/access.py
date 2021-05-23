cc3a9ba58f9dc4ed05068ab206b88f4978b363f6
glat91 <71050732+glat91@users.noreply.github.com>
2021-05-23 17:36:22 -0500

    Update access.py

--- resources/access.py
+++ resources/access.py
@@ -1,6 +1,6 @@
 from flask import request
 from flask_restful import reqparse, fields, Resource
-from models.UserModel import cat_users_new, session
+from models.UserModel import c_users, session
 
 user_args = reqparse.RequestParser()
 user_args.add_argument("user", type=str, help="Need user", required=True)
@@ -9,7 +9,7 @@ user_args.add_argument("password", type=str, help="Need pass", required=True)
 class Access(Resource):
     def post(self):
         args = user_args.parse_args()
-        result = session.query(cat_users_new).filter(cat_users_new.user == args['user']).first()
+        result = session.query(c_users).filter(c_users.user == args['user']).first()
         print(result)
 
         if result == None:
@@ -19,8 +19,8 @@ class Access(Resource):
 
     def put(self):
         r = request.form
-        new_user = cat_users_new(user=r["user"], password=r["password"])
+        new_user = c_users(user=r["user"], password=r["password"])
         # update
-        session.add(cat_users_new)
+        session.add(c_users)
         session.commit()
         return r
