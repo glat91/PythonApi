d38425d35919dbd7f4ccd7d8b55a6b72e00d471f
glat91 <71050732+glat91@users.noreply.github.com>
2021-05-23 17:35:32 -0500

    Update UserModel.py

--- models/UserModel.py
+++ models/UserModel.py
@@ -1,14 +1,14 @@
 from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-
-engine = create_engine("mysql+pymysql://root:@localhost/facturacion")
+# Crear Sesion para conexion a DB
+engine = create_engine("mysql+pymysql://root:@localhost/DB_Name")
 Base = declarative_base()
 Session = sessionmaker(engine)
 session = Session()
-
-class cat_users_new(Base):
-    __tablename__ = "cat_users_new"
+# Modelo usuario
+class c_users(Base):
+    __tablename__ = "c_user"
     id = Column(Integer, primary_key=True)
     user = Column(String(100), nullable=False)
     password = Column(String(100), nullable=False)
